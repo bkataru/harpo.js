60d1af0ca91d8bf040253134223be505e1c8f52b
BK-Modding <kavesbteja@gmail.com>
2019-07-17 19:27:40 +0530

    Refactored code to reduce ambiguity in domain_options and introduced module pattern for FPE algorithms

--- test/index3.js
+++ test/index3.js
@@ -1,24 +1,25 @@
 const crypto = require('crypto');
-const harpo = require('./index');
+const harpo = require('../lib/core/index');
+
+/*
+* 'text': ['ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz', '1234567890',
+        ' ', '.', ',', ';', ':', '/', '?', '!', '@', '#', '$', '%', '&', '*', '(', ')', '[', ']', '-', "'", '"'],
+    'jwt': ['ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz', '1234567890', '.', '_', '-'],
+    'sha-512': ['abcdef', '0123456789']
+*  */
 
 let key = crypto.randomBytes(16).toString('hex');
 let iv = crypto.randomBytes(8).toString('hex');
-
-var options = {
-    input_chars: false,
-    residual_chars: false,
-    preset: 'jwt',
-    ascii_range: {
-        start: 97,
-        end: 112
-    },
+// default: domain of encryption text
+// input: provided domain
+var new_options = {
+    type: 'default', // default, input, preset, ascii-range
+    domain: '',
+    residual_chars: true,
     additional_chars: []
 };
 
-let cipher = harpo.cipher(key, iv, 'aes-256-cbc', {
-    input_chars: false,
-    residual_chars: false,
-});
+let cipher = harpo.cipher(key, iv, 'prefix-cipher', 'aes-256-cbc', new_options);
 
 let text = 'i/am**baala';
 console.log(text);
--- test/index4.js
+++ test/index4.js
@@ -1,10 +1,3 @@
-additional_set = ['abc', '', '', '.', '_', '-'];
+const harpo = require('../lib/core/index');
 
-let domain = [];
-for(var index1 = 0; index1 < additional_set.length; index1++) {
-    for(var index2 = 0; index2 < additional_set[index1].length; index2++) {
-        domain.push(additional_set[index1].split('')[index2]);
-    }
-}
-
-console.log(domain);
+console.log(harpo.presets());
